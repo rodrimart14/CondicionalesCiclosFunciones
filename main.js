dc026866c8caa24cf63e3bee8ce125f164591f9f
rodrimart14 <rodrixeneixe.martinezz@gmail.com>
2023-08-31 18:06:06 -0300

    terminando los ejercicios

---
+++ main.js
@@ -0,0 +1,77 @@
+
+// EJERCICIO 1//
+//for ( let i = 1; i <= 10; i++) {
+    //if (i % 2 === 0) {
+        //console.log(`El numero ${i} es par`);
+        //continue;
+    //} else {
+        //console.log(`El numero ${i} es impar`)
+    //}
+//}
+
+//EJERCICIO 2//
+
+
+
+
+
+//EJERCICIO 3//
+
+//const multiplo = 5;
+
+//for (let i = 0; i <= 40; i++) {
+    //if (i % multiplo === 0) {
+        //console.log(`El numero ${i} es multiplo de ${multiplo}`);
+    //}
+//}
+
+
+//EJERCICIO 4//
+
+//const namber = 10
+
+//for (let i = 0; i <= namber; i++) {
+    //console.log({i})
+//}
+
+
+//EJERCICIO 5//
+
+function sumar(){
+    let palabra = "hola"
+    let number = 5
+        resultado = palabra + number;
+    //console.log("el resultado" + resultado);
+}
+
+
+
+
+
+
+//EJERCICIO 6//
+
+//const miArray = [1, 2, 3, 4, 5, 6];
+    //console.log(miArray);
+
+
+
+//EJERCICIO 7//
+
+//const miArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    //console.log(miArray[0]);
+    //console.log(miArray[1]);
+    //console.log(miArray[2]);
+    //console.log(miArray[3]);
+    //console.log(miArray[5]);
+    //console.log(miArray[6]);
+    //console.log(miArray[7]);
+    //console.log(miArray[8]);
+    //console.log(miArray[9]);
+
+
+
+//EJERCICIO 8//
+
+
